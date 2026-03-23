4014981718a18ff70256a45eb491582eab4a5b03
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-03-23 14:08:31 -0500

    IS THIS HOW IT IS? IS THIS HOW IT'S ALWAYS BEEN?

--- ToDo.cs
+++ ToDo.cs
@@ -2,4 +2,13 @@
 public class ToDo
 {
     // Add your code here
+    public string chores = "Chores";
+    public string[] MarkAsDone = ["laundry", "dishes", "floors"];
+
+    foreach(string[] anything in MarkAsDone) {
+        chores -= anything;
+    }
 }
+
+//should create a method named MarkAsDone that takes an array of todos and returns the modified string array
+//should loop through the todos and prepend the string 'done - ' to each todo
