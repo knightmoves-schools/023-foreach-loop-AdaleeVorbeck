3aaa3b35d14d20d66c2cd7e81d7072c5dec58ba0
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-03-25 13:57:49 -0500

    nfsdksd
    Added properties and logic to manage chores and mark them as done.

--- ToDo.cs
+++ ToDo.cs
@@ -1,5 +1,13 @@
-﻿namespace knightmoves;
 public class ToDo
 {
     // Add your code here
+    public string chores = "Chores";
+    public string[] MarkAsDone = ["laundry", "dishes", "floors"];
+
+    foreach(string[] anything in MarkAsDone) {
+        chores -= anything;
+    }
 }
+
+//should create a method named MarkAsDone that takes an array of todos and returns the modified string array
+//should loop through the todos and prepend the string 'done - ' to each todo
